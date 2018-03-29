a84da76d19cb45b1b45b1e641deb6a9ece3ad904
matheusabreu <matheusabreu.dev@gmail.com>
2018-03-29 10:53:02 -0300

    chore(languages): generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LanguagesComponent } from './languages/languages.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LanguagesComponent,
   ],
   imports: [
     BrowserModule
