b120463b446fee8dc26faed3fe56f4ca54b03b19
matheusabreu <matheusabreu.dev@gmail.com>
2018-03-29 11:33:07 -0300

    chore(language): create model and mock

---
+++ src/app/mocks/language.mock.ts
@@ -0,0 +1,9 @@
+import { Language } from "../models/language.model";
+
+export const LANGUAGES: Language[] = [
+  { id: 11, name: 'Angular' },
+  { id: 12, name: 'Vue' },
+  { id: 13, name: 'React' },
+  { id: 14, name: 'Elm' },
+  { id: 15, name: 'Javascript' },
+];
